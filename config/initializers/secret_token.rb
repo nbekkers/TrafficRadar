5a3ad946616cbced23bebebf082719482633d85b
Nicole Bekkers <hackybekkers@hacklor.com>
2013-05-19 18:37:06 +0200

    Initial commit and add cron job

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrafficRadar::Application.config.secret_token = '6081900602687062e0dc27d46eb8c8fdb2e30625421268d3886afaffb830c12ffba071cbf9bf43850b9ad11ca2a0961524e1ba31ba9da97e22250dae2dddb94c'
