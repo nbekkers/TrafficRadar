5454bb52285145feab84aec33a3bcc5d8124e6dd
Nicole Bekkers <hackybekkers@hacklor.com>
2013-05-21 19:56:10 +0200

    Test to save TrafficLocation when not exists

--- lib/save_traffic_data.rb
+++ lib/save_traffic_data.rb
@@ -1,3 +1,15 @@
 class SaveTrafficData
   
+  def save(traffic)
+    location = TrafficLocation.new
+    location.name = traffic[:location]
+    location.length = traffic[:length]
+    location.travel_time_free_flow = traffic[:travel_time_free_flow]
+    location.velocity = traffic[:velocity]
+    location.travel_time = traffic[:travel_time]
+    location.timestamp = traffic[:timestamp]
+
+    location.save!
+  end
+  
 end
--- test/functional/save_traffic_location_test.rb
+++ test/functional/save_traffic_location_test.rb
@@ -1,12 +1,30 @@
 require 'test_helper'
+require 'save_traffic_data'
 
 class SaveTrafficDataTest < ActiveSupport::TestCase
+  
+  def setup
+    @traffic = {
+      :location => "Location", 
+      :length => 456, 
+      :travel_time_free_flow => 900,
+      :velocity => 30,
+      :travel_time => 990,
+      :timestamp => "20130414170033"
+    }
+  end
 
-  test "save new traffic data" do
+  test "should save new traffic data for new location" do
+    saveTrafficData = SaveTrafficData.new
+    saveTrafficData.save @traffic
     
+    found = TrafficLocation.find(1)
+    assert_equal "Location", found.name
+    assert_equal 456, found.length
+    assert_equal 900, found.travel_time_free_flow
   end
   
-  test "save existing traffic data" do
-  
+  test "should save traffic for existing location" do
+    
   end
 end
