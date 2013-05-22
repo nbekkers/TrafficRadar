e1101ae1b9b3d303fd1585881936b3548355bc14
Nicole Bekkers <hackybekkers@hacklor.com>
2013-05-22 20:09:05 +0200

    Added test to check json response for LocationsController#index

--- test/functional/locations_controller_test.rb
+++ test/functional/locations_controller_test.rb
@@ -1,13 +1,31 @@
 require 'test_helper'
 
 class LocationsControllerTest < ActionController::TestCase
+
   test "should get empty locations when none present" do
     get :index
     assert_response :success
     assert_not_nil assigns(:locations)
   end
   
-  test "should get all locations" do
-    
+  test "should get all locations as json" do
+    save_location
+
+    get :index
+
+    json_response = JSON.parse @response.body
+    assert_equal 1, json_response[0]['id']
+    assert_equal "Location", json_response[0]['name']
+    assert_equal 42, json_response[0]['length']
+    assert_equal 90, json_response[0]['travel_time_free_flow']
+  end
+
+  private
+  def save_location
+    loc = Location.new
+    loc.name = "Location"
+    loc.length = 42
+    loc.travel_time_free_flow = 90
+    loc.save!
   end
 end
