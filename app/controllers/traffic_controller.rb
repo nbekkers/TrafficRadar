1dff3a94f89a964ca8c6a47e77aaf369589ab57c
Nicole Bekkers <hackybekkers@hacklor.com>
2013-05-25 15:24:03 +0200

    TrafficController uses Traffic.find_for_last_hour

--- app/controllers/traffic_controller.rb
+++ app/controllers/traffic_controller.rb
@@ -1,7 +1,7 @@
 class TrafficController < ApplicationController
 
   def index
-    @traffic = Traffic.where(:location_id => params[:location_ids])
+    @traffic = Traffic.find_for_last_hour(params[:location_ids])
     render :json => @traffic
   end
 
