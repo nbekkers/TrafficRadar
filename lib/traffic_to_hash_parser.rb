30d6f513a480c706815f914b1a22abb27c3fde31
Nicole Bekkers <hackybekkers@hacklor.com>
2013-05-26 16:14:03 +0200

    Testing for parsing traffic for multiple locations

--- lib/traffic_to_hash_parser.rb
+++ lib/traffic_to_hash_parser.rb
@@ -4,17 +4,8 @@ class TrafficToHashParser
     @hash_array = []
 
     traffic_data.each do |traffic|
-      traffic_hash = parse_traffic(traffic)
-
-      store_for_location_id(traffic.location_id, traffic_hash)
+      store_for_location_id(traffic.location_id, parse_traffic(traffic))
     end
-    #hash = [{
-    #    :location_id => 1,
-    #    :traffic => [{:timestamp => Time.now, :velocity => 42, :travel_time => 1234}]
-    #},{
-    #    :location_id => 2,
-    #    :traffic => [{:timestamp => Time.now, :velocity => 42, :travel_time => 1234},{:timestamp => Time.now, :velocity => 52, :travel_time => 34}]
-    #}]
 
     @hash_array
   end
@@ -24,6 +15,7 @@ class TrafficToHashParser
     @hash_array.each do |traffic_for_location|
       if traffic_for_location[:location_id] == location_id
         traffic_for_location[:traffic] << traffic_hash
+        return
       end
     end
 
--- test/functional/traffic_to_hash_parser_test.rb
+++ test/functional/traffic_to_hash_parser_test.rb
@@ -2,8 +2,17 @@ require "traffic_to_hash_parser"
 
 class TrafficToHashParserTest < ActiveSupport::TestCase
 
+  LOCATION_ID = 3
+  LOCATION_ID2 = 4
+
   def setup
     @parser = TrafficToHashParser.new
+
+    @timestamp = Time.utc(2013, 5, 6, 12, 30, 45)
+    @timestamp2 = Time.utc(2013, 5, 7, 13, 14, 15)
+
+    @traffic1 = create_traffic(LOCATION_ID, @timestamp, 42, 123, "#dfdfdf")
+    @traffic2 = create_traffic(LOCATION_ID, @timestamp2, 32, 321, "#aeaeae")
   end
 
   def test_getting_empty_hash_when_no_traffic_given
@@ -11,18 +20,40 @@ class TrafficToHashParserTest < ActiveSupport::TestCase
   end
 
   def test_parsing_single_traffic_object
-    timestamp = Time.utc(2013, 5, 6, 12, 30, 45)
-    traffic = create_traffic(3, timestamp, 42, 123, "#dfdfdf")
-
+    traffic = create_traffic(LOCATION_ID, @timestamp, 42, 123, "#dfdfdf")
     traffic_per_location = @parser.parse([traffic])
 
     assert_equal 1, traffic_per_location.size
-    assert_equal 3, traffic_per_location[0][:location_id]
+    assert_equal LOCATION_ID, traffic_per_location[0][:location_id]
     assert_equal 1, traffic_per_location[0][:traffic].size
-    assert_equal "20130506123045", traffic_per_location[0][:traffic][0][:timestamp]
-    assert_equal 42, traffic_per_location[0][:traffic][0][:velocity]
-    assert_equal 123, traffic_per_location[0][:traffic][0][:travel_time]
-    assert_equal "#dfdfdf", traffic_per_location[0][:traffic][0][:color]
+
+    assert_traffic traffic_per_location[0][:traffic][0], "20130506123045", 42, 123, "#dfdfdf"
+  end
+
+  def test_parsing_two_traffic_objects_for_same_location
+    traffic_per_location = @parser.parse([@traffic1, @traffic2])
+
+    assert_equal 1, traffic_per_location.size
+    assert_equal LOCATION_ID, traffic_per_location[0][:location_id]
+    assert_equal 2, traffic_per_location[0][:traffic].size
+
+    assert_traffic traffic_per_location[0][:traffic][0], "20130506123045", 42, 123, "#dfdfdf"
+    assert_traffic traffic_per_location[0][:traffic][1], "20130507131415", 32, 321, "#aeaeae"
+  end
+
+  def test_parsing_two_traffic_objects_for_different_location
+    @traffic2.location_id = LOCATION_ID2
+    traffic_per_location = @parser.parse([@traffic1, @traffic2])
+
+    assert_equal 2, traffic_per_location.size
+    assert_equal LOCATION_ID, traffic_per_location[0][:location_id]
+    assert_equal 1, traffic_per_location[0][:traffic].size
+
+    assert_equal LOCATION_ID2, traffic_per_location[1][:location_id]
+    assert_equal 1, traffic_per_location[1][:traffic].size
+
+    assert_traffic traffic_per_location[0][:traffic][0], "20130506123045", 42, 123, "#dfdfdf"
+    assert_traffic traffic_per_location[1][:traffic][0], "20130507131415", 32, 321, "#aeaeae"
   end
 
   private
@@ -34,6 +65,13 @@ class TrafficToHashParserTest < ActiveSupport::TestCase
     traffic.travel_time = travel_time
     traffic.color = color
     traffic
+  end
+
+  def assert_traffic(traffic, timestamp, velocity, travel_time, color)
+    assert_equal timestamp, traffic[:timestamp]
+    assert_equal velocity, traffic[:velocity]
+    assert_equal travel_time, traffic[:travel_time]
+    assert_equal color, traffic[:color]
   end
 
 end
