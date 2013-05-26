d500c6ae612ac66faf9c97aa7534f3d4a0ba6cbe
Nicole Bekkers <hackybekkers@hacklor.com>
2013-05-26 17:13:57 +0200

    Return location hash with single location when one location given

--- lib/locations_to_hash_converter.rb
+++ lib/locations_to_hash_converter.rb
@@ -1,6 +1,9 @@
 class LocationsToHashConverter
 
   def convert(locations)
-    []
+    loc_array = []
+    locations.each do |loc|
+      loc_array << {:id => loc.id, :name => loc.name}
+    end
   end
 end
--- test/functional/converters/locations_to_hash_converter_test.rb
+++ test/functional/converters/locations_to_hash_converter_test.rb
@@ -3,6 +3,9 @@ require 'locations_to_hash_converter'
 
 class LocationsToHashConverterTest < ActiveSupport::TestCase
 
+  LOCATION_ID = 1
+  LOCATION_NAME = "Loc2"
+
   def setup
     @converter = LocationsToHashConverter.new
   end
@@ -10,6 +13,28 @@ class LocationsToHashConverterTest < ActiveSupport::TestCase
   test "should return empty array for empty locations" do
     locations_hash = @converter.convert []
     assert_equal 0, locations_hash.size
+  end
+
+  test "should return location hash when one location given" do
+    loc = create_location(LOCATION_ID, LOCATION_NAME)
+
+    hash = @converter.convert [loc]
+    assert_equal 1, hash.size
+
+    assert_location LOCATION_ID, LOCATION_NAME, hash[0]
+  end
+
+  private
+  def create_location(id, name)
+    l = Location.new
+    l.id = id
+    l.name = name
+    l
+  end
+
+  def assert_location(id, name, loc_hash)
+    assert_equal id, loc_hash[:id]
+    assert_equal name, loc_hash[:name]
   end
 
 end
