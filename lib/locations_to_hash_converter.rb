35ff2859279134bc3529171f5c9eb79ba7a8036a
Nicole Bekkers <hackybekkers@hacklor.com>
2013-05-26 17:03:03 +0200

    Initialize LocationsToHashConverter

---
+++ lib/locations_to_hash_converter.rb
@@ -0,0 +1,3 @@
+class LocationsToHashConverter
+  
+end
---
+++ test/functional/converters/locations_to_hash_converter_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+require 'locations_to_hash_converter'
+
+class LocationsToHashConverterTest < ActiveSupport::TestCase
+
+  test "should create instance" do
+    converter = LocationsToHashConverter.new
+  end
+end
