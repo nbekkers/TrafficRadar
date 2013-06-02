ef8721da26798b8a2535ff2d8b4e15dc3851281c
Nicole Bekkers <hackybekkers@hacklor.com>
2013-06-02 20:57:38 +0200

    Added index.html, minor changes

--- app/controllers/traffic_controller.rb
+++ app/controllers/traffic_controller.rb
@@ -4,6 +4,7 @@ class TrafficController < ApplicationController
 
   def index
     loc_ids = []
+
     if params[:location_ids]
       loc_ids = params[:location_ids].split(",")
     end
--- lib/converters/locations_to_hash_converter.rb
+++ lib/converters/locations_to_hash_converter.rb
@@ -2,8 +2,14 @@ class LocationsToHashConverter
 
   def convert(locations)
     loc_array = []
+
     locations.each do |loc|
-      loc_array << {:id => loc.id, :name => loc.name, :length => loc.length, :travel_time_free_flow => loc.travel_time_free_flow}
+      loc_array << {
+          :id => loc.id,
+          :name => loc.name,
+          :length => loc.length,
+          :travel_time_free_flow => loc.travel_time_free_flow
+      }
     end
 
     loc_array
